fab14af69ed01aa2211f895ec2b7ad39a9cdbb87
Mahama Hamida <mahamahamida@gmail.com>
2023-10-31 08:51:30 -0700

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/*
+ * binary_tree_node - pointer to a node
+ *
+ * @parent: pointer to the parent node
+ * @value: value to be stored in node
+ *
+ * Return: pointer to the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}

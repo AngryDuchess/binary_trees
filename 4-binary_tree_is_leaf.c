e5edb556fb061bd1f78a33fd22e453d708b6206c
Mahama Hamida <mahamahamida@gmail.com>
2023-11-01 01:44:20 -0700

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node to be checked
+ * Return: 1 is leaf, 0 is no leaf
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if ((node->left == NULL) && (node->right == NULL))
+		return (1);
+	return (0);
+}

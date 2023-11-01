e0e54905ddfdbb838ad2f2e7da3bd500d46f7d27
Mahama Hamida <mahamahamida@gmail.com>
2023-11-01 05:52:42 -0700

    added comments;

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-/**/
+/**
+ * binary_tree_inorder - traverses a tree using inorder traversal
+ *
+ * @tree: pointer to the root node
+ * @func: pointer to a function
+ * Return: Nothing
+ */
 
 void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {

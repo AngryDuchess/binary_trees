46bd6b5d15dfa41c3870c6b15979beb4a21165df
Mahama Hamida <mahamahamida@gmail.com>
2023-11-01 05:50:48 -0700

    7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**/
+
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
+{
+	/*if tree or func is null do nothing*/
+	if (tree == NULL || func == NULL)
+		return;
+	/*pass value stored in nodes in func
+	func(tree->n);*/
+	/*pass each subtree as an argument to inorder function recuresively*/
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}

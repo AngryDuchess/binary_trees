8ca177a76d601282119475e7832625814d24fe53
Mahama Hamida <mahamahamida@gmail.com>
2023-11-02 00:53:47 -0700

    forgot about betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,14 +1,22 @@
 #include "binary_trees.h"
-/**/
+/**
+ * binary_tree_height - calculates the height of a binary tree
+ *
+ * @tree: pointer to the root node
+ * Return: child tree with greatest height
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	/*initialize variables to keep track of the height of each subtree*/
 	size_t left_tree_height = 0;
 	size_t right_tree_height = 0;
 
 	if (tree == NULL)
-		return 0;
-	if (tree-> left != NULL)
+		return (0);
+
+	/*if subtree is not null add 1 to ites height, else add 0*/
+	if (tree->left != NULL)
 		left_tree_height = 1 + binary_tree_height(tree->left);
 	else
 		left_tree_height = 0;
@@ -18,8 +26,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		right_tree_height = 0;
 
+	/*compare both subtrees and return the longest*/
 	if (left_tree_height > right_tree_height)
-		return left_tree_height;
+		return (left_tree_height);
 	else
-		return right_tree_height;
+		return (right_tree_height);
 }

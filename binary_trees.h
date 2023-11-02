a456aaf15a4b5eff1fba7fc1b732094efab704ee
Mahama Hamida <mahamahamida@gmail.com>
2023-11-02 16:19:32 +0000

    forgot about lib file

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,15 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/*other structs*/
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 #endif /* _BINARY_TREES_H_ */
